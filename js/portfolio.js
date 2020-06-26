647b26eedb6856686395d1047a7e6f5755586ff5
otaylor1 <obt1@pitt.edu>
2020-06-26 18:51:01 -0500

    portfolio updates 2020

---
+++ js/portfolio.js
@@ -0,0 +1,39 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("nav-on-scroll");
+    } else {
+      $("#mainNav").removeClass("nav-on-scroll");
+    }
+  });
+
+  $(document).ready(function(){
+    $("#portfolio1btn").click(function(){
+      $("#portfolio1").modal("show");
+    });
+    $("#portfolio2btn").click(function(){
+      $("#portfolio2").modal("show");
+    });
+    $("#portfolio3btn").click(function(){
+      $("#portfolio3").modal("show");
+    });
+    $("#portfolio4btn").click(function(){
+      $("#portfolio4").modal("show");
+    });
+    $("#portfolio5btn").click(function(){
+      $("#portfolio5").modal("show");
+    });
+   
+  });
+
+
+})(jQuery); // End of use strict
+
+
+
+
+
